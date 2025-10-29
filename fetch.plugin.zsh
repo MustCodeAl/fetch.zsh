d87aa2154c0cefb8f44304816b11bbee28c3b1b2
Albert <87888006+MustCodeAl@users.noreply.github.com>
2025-10-28 20:35:24 -0500

    Integrate zoxide with custom cd function
    add zoxide

--- fetch.plugin.zsh
+++ fetch.plugin.zsh
@@ -1,5 +1,17 @@
+# shellcheck disable=all
+# https://github.com/zdharma-continuum/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
 
 
-function cd() {
+check_directory_for_new_repository() {
+
 
 }
+
+if (($+commands[zoxide])); then
+  function cd() {
+    z "$@"
+    check_directory_for_new_repository
+  }
+fi
+
+
